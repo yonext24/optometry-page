2940c92beda451e843de49153a8ae5b20f6f9225
yonext24 <yonielkpo@gmail.com>
2023-09-25 14:39:17 -0300

    .

--- src/utils/prod-dev-variables.js
+++ src/utils/prod-dev-variables.js
@@ -2,5 +2,5 @@
 export const isProduction = process.env.NODE_ENV === 'production'
 
 export const API_ADMIN_URL = isProduction
-  ? 'optometry-back.vercel.app/api/user'
+  ? 'https://optometry-back.vercel.app/api/user'
   : 'http://localhost:5001/api/user'
